b7534b2e55462af713fb33b4723e2188b0f24724
lopesboa <lopesboa1@gmail.com>
2018-11-22 21:39:25 -0200

    Tipagem fraca JS

---
+++ exercicios-js/fundamentos/tipagemFraca.js
@@ -0,0 +1,15 @@
+let meuNome = 'Lopes Boa'
+console.log(meuNome)
+console.log(typeof meuNome)
+
+meuNome = 1.987
+console.log(meuNome)
+console.log(typeof meuNome)
+
+/*
+* Evitar nomes genéricos e siglas 
+* em variáveis ou constantes
+*/
+let valor = ''
+let numero = 1
+let sqn = ''
