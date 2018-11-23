5ac3f2ce1751d0c1295b161d90242c314daac24f
lopesboa <lopesboa1@gmail.com>
2018-11-23 16:11:54 -0200

    Some care about number in JS

--- exercicios-js/fundamentos/numerosAlgunsCuidados.js
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -1,5 +1,7 @@
 console.log(7 / 0)
 console.log("10" / 2)
+console.log("3" + 2) //A String vence(Concatena a string "3" e o numero 2)
+console.log("3" - 2)
 console.log("Show" * 2) // Gera um NaN
 console.log(0.1 + 0.7) // Gera uma imprecisão
 // console.log(10.toString)
