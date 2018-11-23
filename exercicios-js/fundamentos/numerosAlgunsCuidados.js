61d4de059e3dc9cbf691436161b3eccf333539b3
lopesboa <lopesboa1@gmail.com>
2018-11-23 15:09:23 -0200

    Alguns cuidados com numero no JS

--- exercicios-js/fundamentos/numerosAlgunsCuidados.js
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -1,1 +1,6 @@
-
+console.log(7 / 0)
+console.log("10" / 2)
+console.log("Show" * 2) // Gera um NaN
+console.log(0.1 + 0.7) // Gera uma imprecisão
+// console.log(10.toString)
+console.log((10).toFixed(2))
